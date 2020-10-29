c2f49a58f1f1e259aa938d7f1a4b3a53f83d67b2
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-10-29 13:51:21 +0100

    https istedet for http

--- src/js/index.ts
+++ src/js/index.ts
@@ -10,7 +10,7 @@ interface IRecord{
     Duration: number,
     YearOfPublication: string,
 }
-let baseUrl: string = "http://recordrest.azurewebsites.net/api/records"
+let baseUrl: string = "https://recordrest.azurewebsites.net/api/records"
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
